42961bdc20c4454f23ea612a08048e8e00d604af
Danny Simms <djfsimms@hotmail.com>
2023-08-21 21:30:52 -0230

    Adds Game object and board bindEvents method
    Game object has been created to hold the functions required for
    gameplay. startGame, switchPlayer and getActivePlayer have been added
    for gameplay thus far.
    startGame sets player to the first player with the marker x and this
    will show the x marker when the player hovers over each square.
    The Gameboard object also had the method bendEvents added in order to
    listen for when a player clicks a square to add their marker. click
    has only been enabled once to prevent the same square from being chosen
    multiple times.

--- app.js
+++ app.js
@@ -1,39 +1,105 @@
+
+
 (function() {
     const Gameboard = {
-        rows: 3,
-        columns: 3,
-        board: [],
-        createBoard: function() {
-            for (i = 0; i < this.rows; i++) {
-                this.board[i] = [];
-                for (j = 0; j < this.columns; j++) {
-                    this.board[i].push('x');
-                }
-            }
-            console.log(this.board);
-        },
-        cachedDom: function() {
-            this.gameboard = document.getElementById("gameboard");
-            this.gameSquares = document.querySelectorAll(".gameboard-square");
-            console.log(this.gameboard);
-            console.log(this.gameSquares);
-        },
-        render: function () {
-            this.gameSquares.forEach(square => {
-                for (i = 0; i < this.rows; i++) {
-                    for (j = 0; j < this.columns; j++) {
-                        square.classList.add(this.board[i][j])
-                    }
-                }
-            });
-        },
+        gameBoard: [],
         init: function () {
             this.createBoard();
             this.cachedDom();
             this.render();
+            this.bindEvents();
+        },
+        //Creates gameboard as an empty array with 3 columns and 3 rows
+        createBoard: function() {
+            let rows = 3;
+            let columns = 3;
+            for (i = 0; i < this.rows; i++) {
+                this.gameBoard[i] = [];
+                for (j = 0; j < this.columns; j++) {
+                    this.gameBoard[i].push('');
+                }
+            }
+        },
+        //Goes through each column and row of the board array and adds an x in each cell
+        updateBoard: function(marker) {
+            for (i = 0; i < this.rows; i++) {
+                this.gameBoard[i] = [];
+                for (j = 0; j < this.columns; j++) {
+                    this.gameBoard[i].push(marker);
+                }
+            }
+            console.log(this.gameBoard);
+        },
+        cachedDom: function() {
+            this.gameboard = document.getElementById("gameboard");
+            this.gameSquares = document.querySelectorAll(".gameboard-square");
+        },
+        //Goes through the board array to render the board
+        render: function () {
+            this.gameSquares.forEach(square => {
+                for (i = 0; i < this.rows; i++) {
+                    for (j = 0; j < this.columns; j++) {
+                        square.classList.add(this.gameBoard[i][j])
+                    }
+                }
+            });
+        },
+        onClick: function(element) {
+            element.addEventListener('click', (event) => {
+                this.addMarker(event.target);
+                console.log(event);
+            }, { once: true });
+            
+        },
+        bindEvents: function() {
+            this.gameSquares.forEach((square) => this.onClick(square));
+        },
+        setBoardHoverClass: function(activePlayer) {
+            this.gameboard.classList.remove('x');
+            this.gameboard.classList.remove('o');
+            this.gameboard.classList.add(activePlayer.marker);
+        },
+        addMarker: function(square) {
+            const activePlayer = Game.getActivePlayer();
+            square.classList.add(activePlayer.marker);
+            console.log(square);
+            console.log(activePlayer.marker);
         }
+    };
+
+    const Game = {
+        startGame: function() {
+            this.activePlayer = players[0];
+            Gameboard.setBoardHoverClass(this.activePlayer);
+        },
+
+        getActivePlayer: function() {
+            return this.activePlayer;
+        },
+        switchActivePlayer: function() {
+            this.activePlayer = this.activePlayer === players[0] ? players[1] : players[0];
+            Gameboard.setBoardHoverClass(this.activePlayer);
+        },
+    };
+
+    const players = [
+        {
+            name: 'Player One',
+            marker: 'x'
+        },
+        {
+            name: 'Player Two',
+            marker: 'o'
+        }
+
+    ];
+
+    function playGame() {
+        Gameboard.init();
+        Game.startGame()
     }
-    const newBoard = Gameboard.init();
+        Gameboard.init();
+        Game.startGame();
 })();
 
 
